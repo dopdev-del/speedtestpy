e6d40bd10575970d0b907d21384b8c797b6b6a7c
dopdev-del <juniornova90@gmail.com>
2020-07-04 19:24:30 -0400

    tercer commit

--- spdtest.py
+++ spdtest.py
@@ -1,22 +1,25 @@
 import speedtest
+#first we need impot speedtest check it how you can do it in your OS
+
 st = speedtest.Speedtest()
 
-def opciones(option,optione,optiones):
+def testing(option,option1,option2):
 
-	if option == 1 and optione == 2 and optiones == 3:
-		#print(st.download(), 'b/s')
+	if option == 1 and option1 == 2 and option2 == 3:
 		mb=(st.download()*1.0E-6)
-		print("Velocidad de bajada:","{0:.2f}".format(mb),"mb")
-		#print(st.upload(), 'b/s')
+		print("Download speed:","{0:.2f}".format(mb),"mb")
+
 		mb2=(st.upload()*1.0E-6)
-		print("Velocidad de subida:","{0:.2f}".format(mb2),"mb")
+		print("Upload  speed:","{0:.2f}".format(mb2),"mb")
+
 		servernames =[]
 		st.get_servers(servernames)
 		print("Ping:",st.results.ping,'ms')
+
 	else:
 		print("Please enter the correct choice!")
 
 print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
-print("Haciendo prueba de velocidad en Python")
-opciones(1,2,3)
+print("Test in process please wait...")
+testing(1,2,3)
 print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
